f52e159dccb9de30dde6999f38413d71ade3f40a
Martijn Kriek <martijn.kriek@relax-software.nl>
2015-06-28 20:24:37 +0000

    Added ServiceProvider

---
+++ src/Relax/Loader/LoaderServiceProvider.php
@@ -0,0 +1,43 @@
+<?php namespace Way\Generators;
+
+use Illuminate\Support\ServiceProvider;
+use Relax\Loader\FileFinder;
+
+class LoaderServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = true;
+
+	/**
+	 * Booting
+	 */
+	public function boot()
+	{
+		$this->package('relax/loader');
+	}
+
+	/**
+	 * Register the commands
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		$this->app->bind('Relax\Loader\FileFinderInterface', 'Relax\Loader\FileFinder');
+		$this->app->bind('Relax\Loader\FileLoaderInterface', 'Relax\Loader\FileLoader');
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return [];
+	}
+}
